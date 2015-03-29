01f6757cdace581658246459f2da6a9be6513b54
Brett Viren <brett.viren@gmail.com>
2015-03-29 17:58:12 -0400

    Post install config works.

--- scripts/update-target.sh
+++ scripts/update-target.sh
@@ -3,15 +3,7 @@
 srcdir=$(dirname $(readlink -f $BASH_SOURCE))
 source $srcdir/common.sh
 
-# this key must match what's used in the nbica-target-init.yml playbook
-ssh_key="$topdir/keys/ssh-key"
-if [ ! -f "$ssh_key" ] ; then
-    mkdir -p "$topdir/keys"
-    echo "Making SSH key pair root@ and rootless@"
-    ssh-keygen -t rsa -f "$ssh_key"
-fi
-
-play -K nbica-target-init
+play nbica-target-init -K -k
 
 echo "IMPORTANT: now move hosts out of [toconfigure] from $hostfile"
 
